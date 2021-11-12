9f1665343b0b270bf44981f8bf5ca97e9e0f78b4
Hugo Yanez <hugodavidyanez@gmail.com>
2021-11-11 23:19:32 -0500

    Created GET requests in htmlRouter.js

--- Develop/routes/htmlRouter.js
+++ Develop/routes/htmlRouter.js
@@ -2,3 +2,13 @@ const util = require("util");
 const router = require("express").Router();
 const fs = require("fs");
 const path = require("path");
+
+// GET /notes should return the `notes.html` file.
+router.get("/notes", (req, res) =>
+  res.sendFile(path.join(__dirname, "../public/notes.html"))
+);
+
+// GET * should return the `index.html` file.
+router.get("/*", (req, res) =>
+  res.sendFile(path.join(__dirname, "../public/index.html"))
+);
