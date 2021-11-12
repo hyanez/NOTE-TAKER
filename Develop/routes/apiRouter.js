feb142baf7b2a2009191466cd958bba6b2a1174e
Hugo Yanez <hugodavidyanez@gmail.com>
2021-11-11 23:30:07 -0500

    Initialized dependencies in apiRouter.js

--- Develop/routes/apiRouter.js
+++ Develop/routes/apiRouter.js
@@ -1,1 +1,4 @@
-
+const util = require("util");
+const router = require("express").Router();
+const fs = require("fs");
+const Notes = require("../db/db.json");
--- Develop/routes/htmlRouter.js
+++ Develop/routes/htmlRouter.js
@@ -4,13 +4,15 @@ const fs = require("fs");
 const path = require("path");
 
 // GET /notes should return the `notes.html` file.
-router.get("/notes", (req, res) =>
-  res.sendFile(path.join(__dirname, "../public/notes.html"))
-);
+router.get("/notes", (req, res) => {
+  console.info(`${req.method} request received for notes.html`);
+  res.sendFile(path.join(__dirname, "../public/notes.html"));
+});
 
 // GET * should return the `index.html` file.
-router.get("/*", (req, res) =>
-  res.sendFile(path.join(__dirname, "../public/index.html"))
-);
+router.get("/*", (req, res) => {
+  console.info(`${req.method} request received for index.html`);
+  res.sendFile(path.join(__dirname, "../public/index.html"));
+});
 
 module.exports = router;
