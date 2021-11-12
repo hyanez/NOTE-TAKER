cbef1011b6e03141ccb4e37a39e07e3d70b910f5
Hugo Yanez <hugodavidyanez@gmail.com>
2021-11-11 23:15:33 -0500

    Inititalized dependencies in htmlRouter.js

--- Develop/routes/apiRouter.js
+++ Develop/routes/apiRouter.js
@@ -1,9 +1,1 @@
-//`GET /notes` should return the `notes.html` file.
-app.get("/notes", (req, res) =>
-  res.sendFile(path.join(__dirname, "./public/notes.html"))
-);
 
-// `GET *` should return the `index.html` file.
-app.get("/*", (req, res) =>
-  res.sendFile(path.join(__dirname, "./public/index.html"))
-);
---
+++ Develop/routes/htmlRouter.js
@@ -0,0 +1,4 @@
+const util = require("util");
+const router = require("express").Router();
+const fs = require("fs");
+const path = require("path");
