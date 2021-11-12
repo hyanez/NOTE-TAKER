7caba17b99b6aeac48a3770ab98a99f8724b851c
Hugo Yanez <hugodavidyanez@gmail.com>
2021-11-11 23:02:27 -0500

    Created middleware for routes htmlRouter and apiRouter

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+//Dependencies
 const express = require("express");
 const path = require("path");
 const fs = require("fs");
@@ -5,3 +6,16 @@ const apiRouter = require("./routes/apiRouter.js");
 const htmlRouter = require("./routes/htmlRouter.js");
 
 const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+//Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static("public"));
+
+//Middleware for htmlRouter
+app.use(htmlRouter);
+//Middleware for apiRouter
+app.use(apiRouter);
