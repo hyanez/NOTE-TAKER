563c2bc787d38c0c533ea86d8bcb10577082f862
Hugo Yanez <hugodavidyanez@gmail.com>
2021-11-11 22:56:24 -0500

    Created htmlRouter.js and server.js

---
+++ server.js
@@ -0,0 +1,5 @@
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
+const apiRouter = require("./routes/apiRouter.js");
+const htmlRouter = require("./routes/htmlRouter.js");
