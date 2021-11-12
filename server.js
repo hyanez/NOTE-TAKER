dbc81d84e2be6f3e7a386d032bf43ad76ee95d85
Hugo Yanez <hugodavidyanez@gmail.com>
2021-11-11 22:57:23 -0500

    Initialized dependencies in server.js

--- server.js
+++ server.js
@@ -3,3 +3,5 @@ const path = require("path");
 const fs = require("fs");
 const apiRouter = require("./routes/apiRouter.js");
 const htmlRouter = require("./routes/htmlRouter.js");
+
+const PORT = process.env.PORT || 3001;
